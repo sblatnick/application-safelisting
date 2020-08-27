4d32f91c75e628d9b68039da421c96cc0ce6fd62
naew6552 <naew6552@colorado.edu>
2020-08-27 13:37:34 -0700

    Semi-working overwrite of read systemcall

--- asl.c
+++ asl.c
@@ -10,36 +10,38 @@ static char *sym_name = "sys_call_table";
 
 typedef asmlinkage long (*sys_call_ptr_t)(const struct pt_regs *);
 static sys_call_ptr_t *sys_call_table;
-typedef asmlinkage long (*custom_write) (const char __user *filename, int flags, umode_t mode);
+typedef asmlinkage long (*custom_open) (const char __user *filename, int flags, umode_t mode);
 
-custom_write old_write;
+MODULE_LICENSE("GPL");
 
-static asmlinkage long my_write(const char __user *filename, int flags, umode_t mode)
+custom_open old_open;
+
+static asmlinkage long my_open(const char __user *filename, int flags, umode_t mode)
 {
-  printk(KERN_INFO "ASL: Attempted Write!");
-  return old_write(filename, flags, mode);
+  printk(KERN_INFO "ASL: %s\n", filename);
+  return old_open(filename, flags, mode);
 }
 
 static int __init init_asl(void)
 {
-  print(KERN_INFO "ASL: beginning overwite of sys_calL_table");
+  printk(KERN_INFO "ASL: beginning overwite of sys_calL_table");
 
   // Retrieve system call table pointer
   sys_call_table = (sys_call_ptr_t *)kallsyms_lookup_name(sym_name);
   
   // Store old write syscall
-  old_write = (custom_write)sys_call_table[__NR_write];
+  old_open = (custom_open)sys_call_table[3];
 
   // Temporarily disable write protection
   write_cr0(read_cr0() & (~0x10000));
 
   // Overwrite the syscall table entry
-  sys_call_table[__NR_write] = (sys_call_ptr_t)my_write;
+  sys_call_table[3] = (sys_call_ptr_t)my_open;
 
   // Re-enable write protection
   write_cr0(read_cr0() | 0x10000);
 
-  print(KERN_INFO "ASL: end overwite of sys_calL_table");
+  printk(KERN_INFO "ASL: end overwite of sys_calL_table");
 
   return 0;
 }
@@ -50,7 +52,7 @@ static void __exit cleanup_asl(void)
   write_cr0(read_cr0() & (~0x10000));
 
   // Overwrite the syscall table entry with old write
-  sys_call_table[__NR_write] = (sys_call_ptr_t)old_write;
+  sys_call_table[3] = (sys_call_ptr_t)old_open;
 
   // Re-enable write protection
   write_cr0(read_cr0() | 0x10000);
